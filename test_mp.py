ec7e6010cffcc52fc56436413a79d0546804f934
yelong shen <shengyelong@gmail.com>
2024-10-10 20:29:08 -0700

    mp

--- test_mp.py
+++ test_mp.py
@@ -3,7 +3,29 @@ import torch
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+def setup(rank, world_size):
+    # Initialize the process group (NCCL is preferred for GPU communication)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def get_init_model(rank):
+    # rank in predictor mode.
+    if rank == 0:
+        model = torch.nn.Linear(4, 16).to(rank)
+    # rank in trainer model.
+    elif rank == 1:
+        model = torch.nn.Linear(4, 16).to(rank)
+
 if __name__ == "__main__":
     world_size = 2
     rank = int(os.environ['RANK'])
-    print(rank)
+
+    num_pred_gpus = 1
+    num_trainer_gpus = 1
+
+    print('gpu rank', rank)
+    model = get_init_model(rank) #
+
+    print('done!')
