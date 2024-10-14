12c60b71c4ba33937fff3f330b42a2fbfce2a37f
yelong shen <shengyelong@gmail.com>
2024-10-14 14:11:54 -0700

    lllfeffeffef

---
+++ code_contests_test.py
@@ -0,0 +1,31 @@
+import os
+import io
+import pickle
+import traceback
+import copy
+import datetime
+from typing import Any, Dict, Optional
+from concurrent.futures import TimeoutError
+from functools import partial
+from contextlib import redirect_stdout
+import sys
+
+from datasets import load_dataset
+
+
+def _test():
+    dataset = load_dataset("deepmind/code_contests")
+    train = dataset['train']
+
+    
+    for i in range(0, len(train)):
+        example = train[i]
+        soluts = example['solutions']
+        for (lang, code) in zip(soluts['language'], soluts['solution']):
+            if lang == 3:
+                print(example['description'], code)
+                break
+
+
+if __name__ == '__main__':
+    _test()
