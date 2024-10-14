b7930ecf023f397d753f2c9163961d80f76c74d4
yelong shen <shengyelong@gmail.com>
2024-10-14 14:59:41 -0700

    12413980gsdfgef

--- code_contests_test.py
+++ code_contests_test.py
@@ -14,21 +14,38 @@ from datasets import load_dataset
 
 
 def _test():
+    old_stdin = sys.stdin
+    
     dataset = load_dataset("deepmind/code_contests")
     train = dataset['train']
 
-    idx = 0
+    status = 0
     for i in range(0, len(train)):
         example = train[i]
         soluts = example['solutions']
+        pycode = ''
         for (lang, code) in zip(soluts['language'], soluts['solution']):
             if lang == 3:
                 print(example['description'], code)
-                idx = 1
+                pycode = code
+                status = 1
                 break
+        tests = example['public_tests']
         
-        if idx == 1:
-            break
+        if status == 1:
+            for test_input, test_output in zip(tests['input'], tests['output']):
+                old_stdout = sys.stdout
+                sys.stdout = io.StringIO()
+                
+                sys.stdin = io.StringIO(test_input)
+                exec(pycode)
+                output = sys.stdout.getvalue()
 
+                sys.stdout = old_stdout
+
+                print(test_output, output)
+
+        if status == 1:
+            break
 if __name__ == '__main__':
     _test()
