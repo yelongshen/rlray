645ba35976f4d152ac4716ff27b24daad0cfc0d4
yelong shen <shengyelong@gmail.com>
2024-10-28 17:52:18 -0700

    add vllm example

--- mini_rl_example.py
+++ mini_rl_example.py
@@ -11,29 +11,39 @@ import time
 import random
 
 from vllm import LLM, SamplingParams
+from typing import Any, Dict, Optional
+from concurrent.futures import TimeoutError
+from functools import partial
+from contextlib import redirect_stdout
+import sys
 
+from datasets import load_dataset
+
+def play():
+    # Load a model
+    llm = LLM(model="facebook/opt-6.7b")  # You can specify any Hugging Face model here
+    # Set sampling parameters
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.9, max_tokens=50)
+
+    dataset = load_dataset("deepmind/code_contests")
+    train = dataset['train']
+
+    outputs = []
+    for i in range(0, len(train)):
+        example = train[i]
+        soluts = example['solutions']
+        problem = example['description']
+
+        ans = llm.generate([problem], sampling_params)
+        print(ans)
+        outputs.append(ans)
 
 def main():
     # system parameters:
     # args.ngpu_per_node
     # args.nnode_actor
     # args.nnode_learner
-    
-    # Load a model
-    llm = LLM(model="facebook/opt-6.7b")  # You can specify any Hugging Face model here
-
-    # Define your prompt
-    prompt = "Once upon a time in a futuristic world,"
-
-    # Set sampling parameters
-    sampling_params = SamplingParams(temperature=0.7, top_p=0.9, max_tokens=50)
-
-    # Run inference
-    output = llm.generate([prompt], sampling_params)
-
-    # Print the output
-    print(output)
-
+    play()
 
 if __name__ == "__main__":
     main()
