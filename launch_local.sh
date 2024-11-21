7eff7ef6d846e1bf4d90d36f0fcf3f346e5eb027
yelong shen <shengyelong@gmail.com>
2024-11-21 11:40:32 -0800

    local git

--- launch_local.sh
+++ launch_local.sh
@@ -3,3 +3,5 @@ python -m torch.distributed.launch --nproc_per_node=2 test_mp.py
 python -m torch.distributed.launch --nproc_per_node=8 mini_rl_example.py 
 
 python -m torch.distributed.launch --nproc_per_node=4 test_mp2.py 
+
+bash /gpfs/public/pretrain/yale/rlray/launch_dist.sh
