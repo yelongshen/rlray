6cddf5abeaeac5fa67908497e80abceb6f50518e
yelongshen <yeshe@microsoft.com>
2024-12-01 16:58:31 -0800

    Update mini_rl_example3.py

--- mini_rl_example3.py
+++ mini_rl_example3.py
@@ -288,12 +288,13 @@ def learn():
     while step < 40000:
         l = len(buffer) if rank == buffer_rank else rpc.rpc_sync(f"worker-{buffer_rank}", len_buffer) #rev_experience_len('worker2')
         if l > 20:
+            torch.cuda.empty_cache()
+            
             data = buffer.sample(batch_size) if rank == buffer_rank else rpc.rpc_sync(f"worker-{buffer_rank}", pop_from_buffer, args=(batch_size, )) #rev_experience_data('worker2', 2)
             text = [d[0] for d in data]
             score = [d[1] for d in data]
             
             inputs = tokenizer(text, add_special_tokens=True, padding=True, truncation=True, return_tensors="pt").to(device)
-
             if inputs["input_ids"].shape[1] > 4096:
                 continue
 
