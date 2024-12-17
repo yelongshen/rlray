dcee6e00bb2b995f18af809387a0535b8dc24d84
yelongshen <yeshe@microsoft.com>
2024-12-16 22:32:33 -0800

    Update phimodel.py

--- phimodel.py
+++ phimodel.py
@@ -39,7 +39,7 @@ logger = logging.get_logger(__name__)
 # if is_flash_attn_2_available():
 _flash_supports_window_size = False
 try:
-    from flash_attn import flash_attn_func, flash_attn_varlen_func
+    from flash_attn import flash_attn_func #, flash_attn_varlen_func
     #from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
     _flash_supports_window_size = "window_size" in list(inspect.signature(flash_attn_func).parameters)
 except ImportError as error:
