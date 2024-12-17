36138a4dd04ff735f8a46ff19db94d56925ab9a1
yelongshen <yeshe@microsoft.com>
2024-12-16 23:58:26 -0800

    Update phimodel.py

--- phimodel.py
+++ phimodel.py
@@ -763,7 +763,7 @@ class _Phi3Model(_Phi3PreTrainedModel):
         for layer_idx, decoder_layer in enumerate(self.layers):
             #if output_hidden_states:
             #    all_hidden_states += (hidden_states,)
-            print('layer_idx', layer_idx, 'hidden_state.dtype', hidden_states.dtype, 'decoder_layer.dtype', decoder_layer.self_attn.qkv_proj.weight.dtype)
+            #print('layer_idx', layer_idx, 'hidden_state.dtype', hidden_states.dtype, 'decoder_layer.dtype', decoder_layer.self_attn.qkv_proj.weight.dtype)
 
             kv_cache = None
             if past_key_values is not None:
