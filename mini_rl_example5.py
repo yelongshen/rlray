eb65b8eb4fac2eac4aa01a0f05af3328df622e19
yelongshen <yeshe@microsoft.com>
2025-01-14 10:25:22 -0800

    Update mini_rl_example5.py

--- mini_rl_example5.py
+++ mini_rl_example5.py
@@ -418,7 +418,8 @@ def learn(learndp): #, mdg):
             torch.cuda.empty_cache()
             data = buffer.sample(batch_size) if rank == buffer_rank else rpc.rpc_sync(f"worker-{buffer_rank}", pop_from_buffer, args=(batch_size, ), timeout=0) #rev_experience_data('worker2', 2)
             print('done with fetching the data', rank)
-            
+            dist.barrier(learndp)
+            print('getting all data done...', rank)
             _tokens = [d[0] for d in data]
             _masks = [d[1] for d in data]
             _probs = [d[2] for d in data]
