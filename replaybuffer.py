8103ef07c917aace41943757815d6f699949cd1e
yelongshen <yeshe@microsoft.com>
2025-02-03 18:59:17 -0800

    Create replaybuffer.py

---
+++ replaybuffer.py
@@ -0,0 +1,121 @@
+
+import os
+import sys
+
+import torch
+import torch.distributed as dist
+import torch.distributed.rpc as rpc
+from queue import Queue
+import threading
+import time
+import random
+import argparse
+
+from typing import List, Optional, Tuple, Union
+
+import torch.nn as nn
+
+#from vllm import LLM, SamplingParams
+
+from typing import Any, Dict, Optional
+from concurrent.futures import TimeoutError
+from functools import partial
+#from contextlib import redirect_stdout
+import sys
+
+
+#from datasets import load_dataset
+
+import torch 
+import logging
+
+#from peft import LoraConfig
+#from trl import SFTTrainer
+#from transformers import TrainingArguments, BitsAndBytesConfig
+from accelerate import Accelerator
+from torch.utils.data import DataLoader
+
+import os
+import io
+import pickle
+import traceback
+import copy
+import datetime
+from typing import Any, Dict, Optional
+from functools import partial
+import sys
+
+
+import signal
+from dataclasses import dataclass
+
+
+import re
+
+from collections import deque
+
+import numpy as np
+
+# ReplayBuffer 
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = deque(maxlen=capacity) # every sample is very different. 
+        self.epsilon = 1e-8
+        self.alpha = 0.01       
+        self.lock = threading.Lock()
+
+    # experience : #<prompt, response, reward, probs, crits, tokens, masks, seq_rewards>
+    def push(self, experience):
+        """ Add new experience to the buffer """
+        with self.lock:
+            self.buffer.append(experience) 
+            
+    def clear(self):
+        self.buffer.clear()
+
+    def pop(self, batch_size):
+        """
+        Pop the oldest batch of experiences from the buffer (FIFO order).
+        Args:
+            batch_size (int): Number of experiences to pop.
+        Returns:
+            List of popped experiences.
+        """
+        with self.lock:
+            batch_size = min(batch_size, len(self.buffer))  # Ensure we don't pop more than available
+            data = [self.buffer.popleft() for _ in range(batch_size)]  # Pop oldest elements
+            return data
+        
+    def __len__(self):
+        with self.lock:
+            return len(self.buffer)
+
+    def get_rewards(self):
+        with self.lock:
+            rewards = []
+            for d in self.buffer:
+                prompt, response, reward, probs, crits, tokens, masks, seq_rewards = d
+                rewards.append(reward)
+            return rewards
+            
+    def mean_reward(self):
+        rewards = self.get_rewards()
+        return np.mean(rewards)
+
+    def avg_responselen(self):
+        with self.lock:
+            response_len = []
+            for d in self.buffer:
+                prompt, response, reward, probs, crits, tokens, masks, seq_rewards = d
+                response_len.append(len(probs))
+            return np.mean(response_len)
+        
+    def z_score_normalization(self):
+        """Standardize rewards using mean and standard deviation."""
+        rewards = self.get_rewards()
+        
+        mean = np.mean(rewards)
+        std = np.std(rewards) + self.epsilon
+        return (rewards - mean) / std
+
