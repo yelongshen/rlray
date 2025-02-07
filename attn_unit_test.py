9187deb66606ae481120c61910fe33d482318a6a
yelongshen <yeshe@microsoft.com>
2025-02-06 16:40:24 -0800

    Update attn_unit_test.py

--- attn_unit_test.py
+++ attn_unit_test.py
@@ -34,6 +34,22 @@ def vanilla_attention():
     print(attn_output.shape)
     return attn_output
 
+def vanilla_sliding_attention():
+    attn_weights = torch.matmul(query, key.transpose(2, 3)) / math.sqrt(head_dim)
+    attention_mask = torch.tril(torch.ones((seq_len, seq_len), device=device))
+    for i in range(seq_len):
+        start = max(i - window + 1, 0)
+        attention_mask[i][:start] = 0
+        
+    attn_weights = attn_weights.masked_fill(attention_mask < 0.1, float('-inf'))
+    
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(value.dtype)
+    attn_output = torch.matmul(attn_weights, value)
+    
+    print(attn_output.shape)
+    return attn_output
+
+
 def flash_attention():
     f_query = query.transpose(1, 2).contiguous()
     f_key = key.transpose(1, 2).contiguous()
@@ -50,15 +66,44 @@ def flash_attention():
     attn_output = attn_output.transpose(1, 2).contiguous()
     print(attn_output.shape)
     return attn_output
+    
+def flash_sliding_attention():
+    f_query = query.transpose(1, 2).contiguous()
+    f_key = key.transpose(1, 2).contiguous()
+    f_value = value.transpose(1, 2).contiguous()
+
+    attn_output = flash_attn_func(
+            f_query,
+            f_key,
+            f_value,
+            0,
+            softmax_scale=None,
+            causal=True, 
+            window_size=(
+                window,
+                window,
+            )
+    )
+    
+    attn_output = attn_output.transpose(1, 2).contiguous()
+    print(attn_output.shape)
+    return attn_output
+    
+    
+    
+
 
 #torch.Size([2, 6, 16, 8])
 #torch.Size([2, 16, 6, 8])
 
-o1 = vanilla_attention()
-o2 = flash_attention()
+#o1 = vanilla_attention()
+#o2 = flash_attention()
+
+o1 = vanilla_sliding_attention()
+o2 = flash_sliding_attention()
 
 print(o1)
 print(o2)
-assert torch.allclose(o1, o2, atol=1e-4)
+assert torch.allclose(o1, o2, atol=1e-2)
 
 #def flash_attn
