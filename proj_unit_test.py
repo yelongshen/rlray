0dedb55d8b85cba577e60be513a730e078d1b021
yelongshen <yeshe@microsoft.com>
2025-02-10 00:25:32 -0800

    Update proj_unit_test.py

--- proj_unit_test.py
+++ proj_unit_test.py
@@ -34,20 +34,49 @@ def case1():
     if in_proj.bias is not None:
         xz = xz + rearrange(in_proj.bias.to(dtype=xz.dtype), "d -> d 1")
 
+    x, z = xz.chunk(2, dim=1)
     # if not self.training:
     #     xz = xz.to(torch.float32)
     #A = -torch.exp(self.A_log.float())  # (d_inner, d_state)
-    return xz
+    return x, z
 
 def case2():
     xz = in_proj(hidden_states.to(dtype = in_proj.weight.dtype).squeeze(1))  # (B 2D)
-    return xz
+
+    #xz = self.in_proj(hidden_states.to(dtype = self.in_proj.weight.dtype).squeeze(1))  # (B 2D)
+    x, z = xz.chunk(2, dim=-1)  # (B D)
+
+    return x, z 
+    #x, z = xz.chunk(2, dim=-1)  # (B D)
+
+    #if have conv_state
+    #x = causal_conv1d_update(
+    #            x,
+    #            conv_state,
+    #            rearrange(self.conv1d.weight, "d 1 w -> d w"),
+    #            self.conv1d.bias,
+    #            self.activation,
+    #        )
+    
+
+    #return xz
 
 
-o1 = case1()
-o2 = case2()
+x1, z1 = case1()
+x2, z2 = case2()
 
-print(o1)
-print(o2)
-assert torch.allclose(o1, o2, atol=1e-2)
+print(x1.shape)
+print(x1)
+
+print(x2.shape)
+print(x2)
+
+print(z1.shape)
+print(z1)
+
+print(z2.shape)
+print(z2)
+
+assert torch.allclose(x1, x2, atol=1e-2)
+assert torch.allclose(z1, z2, atol=1e-2)
 
