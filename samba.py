e67b031a04c5c4403f35ad94ac3ce1e76f9faf61
yelongshen <yeshe@microsoft.com>
2025-02-09 21:22:05 -0800

    Update samba.py

--- samba.py
+++ samba.py
@@ -35,23 +35,42 @@ from torch.utils.checkpoint import checkpoint
 
 logger = logging.get_logger(__name__)
 
-# Transformers scans dependencies in the modeling file, causing issues on conditional loading. The regex only ignores try/catch blocks, but not if statements
-# if is_flash_attn_2_available():
-_flash_supports_window_size = False
+
+
 try:
-    from flash_attn import flash_attn_func #, flash_attn_varlen_func
+    from flash_attn import flash_attn_func, flash_attn_varlen_func
     #from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
     _flash_supports_window_size = "window_size" in list(inspect.signature(flash_attn_func).parameters)
-except ImportError as error:
-    logger.warning(
-        f"`flash-attention` package not found, consider installing for better performance: {error}."
-    )
     if not _flash_supports_window_size:
-        logger.warning(
-            "Current `flash-attention` does not support `window_size`. Either upgrade or use `attn_implementation='eager'`."
-        )
+        raise ValueError("Please update flash-attention to support window size.")
+    from flash_attn.ops.activations import swiglu
+    from flash_attn.ops.rms_norm import RMSNorm #as RMSNorm
+except ImportError:
+    logger.warning("Flash submodules not found, consider installing for better performance.")
+    swiglu = None
+    RMSNorm = None
 
-class _Phi3RMSNorm(nn.Module):
+try:
+    from causal_conv1d import causal_conv1d_fn, causal_conv1d_update
+except ImportError:
+    logger.warning("Causal Conv1d submodules not found, consider installing for better performance.")
+    causal_conv1d_fn, causal_conv1d_update = None
+
+import causal_conv1d_cuda
+try:
+    from mamba_ssm.ops.triton.selective_state_update import selective_state_update
+except ImportError:
+    logger.warning("Selective state updating submodules not found, consider installing for better performance.")
+    selective_state_update = None
+
+try:
+    from mamba_ssm.ops.triton.layernorm import RMSNorm, layer_norm_fn, rms_norm_fn
+except ImportError:
+    logger.warning("Triton LayerNorm submodules not found, consider installing for better performance.")
+    RMSNorm, layer_norm_fn, rms_norm_fn = None, None, None
+from torch.cuda.amp import custom_bwd, custom_fwd
+
+class _SambaRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         """
         Phi3RMSNorm is equivalent to T5LayerNorm
@@ -66,6 +85,8 @@ class _Phi3RMSNorm(nn.Module):
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
         return self.weight * hidden_states.to(input_dtype)
+
+PHI_NORM_CLASS = _SambaRMSNorm if RMSNorm is None else RMSNorm
 
 def _get_unpad_data(attention_mask):
     seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
@@ -620,39 +641,22 @@ class _Phi3DecoderLayer(nn.Module):
         return hidden_states, key_cache, value_cache
 
 
-class _Phi3PreTrainedModel(nn.Module):
-    def __init__(self, config: Phi3Config):
+class _SambaPreTrainedModel(nn.Module):
+    def __init__(self, config):
         super().__init__()
-        self.config_class = Phi3Config
         self.base_model_prefix = "model"
         self.supports_gradient_checkpointing = True
-        self._no_split_modules = ["Phi3DecoderLayer"]
-        self._skip_keys_device_placement = "past_key_values"
+        self._no_split_modules = ["_SambaDecoderLayer"]
+        self._skip_keys_device_placement = "past_caches"
         self._supports_flash_attn_2 = True
         self._supports_sdpa = False
         self._supports_cache_class = True
         self._version = "0.0.5"
         self.config = config
-        
-    def _init_weights(self, module):
-        std = self.config.initializer_range
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
 
 
-class _Phi3Model(_Phi3PreTrainedModel):
-    """
-    Transformer decoder consisting of *config.num_hidden_layers* layers. Each layer is a [`Phi3DecoderLayer`]
-    Args:
-        config: Phi3Config
-    """
-    def __init__(self, config: Phi3Config):
+class _SambaModel(_SambaPreTrainedModel):        
+    def __init__(self, config):
         super().__init__(config)
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
@@ -660,35 +664,28 @@ class _Phi3Model(_Phi3PreTrainedModel):
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
         self.embed_dropout = nn.Dropout(config.embd_pdrop)
         self.layers = nn.ModuleList(
-            [_Phi3DecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
+            [_SambaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
         )
+        self.final_layernorm = PHI_NORM_CLASS(config.hidden_size, eps=config.layer_norm_eps)
         self._attn_implementation = config._attn_implementation
-        self.norm = _Phi3RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-
         self.gradient_checkpointing = False
-        # Initialize weights and apply final processing
-        # self.post_init()
+
     def get_input_embeddings(self):
         return self.embed_tokens
 
     def set_input_embeddings(self, value):
         self.embed_tokens = value
 
+    
     def forward(
         self,
         input_ids: torch.LongTensor = None,
-        #attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[List[Tuple[torch.FloatTensor, torch.FloatTensor]]] = None,
-        #inputs_embeds: Optional[torch.FloatTensor] = None,
-        #use_cache: Optional[bool] = None,
-        #output_attentions: Optional[bool] = None,
-        #output_hidden_states: Optional[bool] = None,
-        #return_dict: Optional[bool] = None,
+        past_caches = None, 
+        inference_mode = False,
     ):  
         batch_size, seq_length = input_ids.shape[:2]
-        past_key_values_length = 0 if past_key_values is None else past_key_values[0][0].shape[-2]
-
+        if past_length = 0 if past_caches is None else past_caches[0]
+        
         device = input_ids.device
         
         if position_ids is None:
@@ -786,6 +783,157 @@ class _Phi3Model(_Phi3PreTrainedModel):
         #)
         # return lastlayer hidden states, and KV cache.
         return hidden_states, next_decoder_cache
+        
+    
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        # print(use_cache)
+        # # use_cache = False
+        # print(input_ids)
+        # print(attention_mask)
+        # print(position_ids)
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # retrieve input_ids and inputs_embeds
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            batch_size, seq_length = input_ids.shape[:2]
+        elif inputs_embeds is not None:
+            batch_size, seq_length = inputs_embeds.shape[:2]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        past_key_values_length = 0
+
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
+        if use_cache:
+            use_legacy_cache = not isinstance(past_key_values, Cache)
+            if use_legacy_cache:
+                past_key_values = Phi3DynamicCache.from_legacy_cache(past_key_values)
+            past_key_values_length = past_key_values.get_usable_length(seq_length)
+
+        if position_ids is None:
+            device = input_ids.device if input_ids is not None else inputs_embeds.device
+            position_ids = torch.arange(
+                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
+            )
+            position_ids = position_ids.unsqueeze(0).view(-1, seq_length)
+        else:
+            position_ids = position_ids.view(-1, seq_length).long()
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        if attention_mask is not None and self._attn_implementation == "flash_attention_2" and use_cache:
+            is_padding_right = attention_mask[:, -1].sum().item() != batch_size
+            if is_padding_right:
+                raise ValueError(
+                    "You are attempting to perform batched generation with padding_side='right'"
+                    " this may lead to unexpected behaviour for Flash Attention version of Samba. Make sure to "
+                    " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
+                )
+
+        if self._attn_implementation == "flash_attention_2":
+            # 2d mask is passed through the layers
+            attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
+        elif self._attn_implementation == "sdpa" and not output_attentions:
+            # output_attentions=True can not be supported when using SDPA, and we fall back on
+            # the manual implementation that requires a 4D causal mask in all cases.
+            attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
+            )
+        else:
+            # 4d mask is passed through the layers
+            attention_mask = _prepare_4d_causal_attention_mask(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
+                sliding_window=self.config.sliding_window,
+            )
+
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+
+        for decoder_layer in self.layers:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                )
+
+            hidden_states = layer_outputs[0]
+
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        hidden_states = self.final_layernorm(hidden_states.to(dtype=self.final_layernorm.weight.dtype))
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = next_decoder_cache.to_legacy_cache() if use_legacy_cache else next_decoder_cache
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
+
 
 import torch.nn.functional as F
 
