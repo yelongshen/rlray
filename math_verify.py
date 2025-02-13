bc18933de21a58fc8e34a7058b001bdd3497c463
yelongshen <yeshe@microsoft.com>
2025-02-13 00:30:01 -0800

    Create math_verify.py

---
+++ math_verify.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import subprocess
+import pickle
+
+def math_verify(answer, gold):
+    command = ['python3.12', '-c', f'from math_verify import parse, verify; import pickle; import sys; pickle.dump(verify(parse("{answer}"), parse("{gold}")), sys.stdout.buffer)']
+    o = False
+    with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE) as proc:
+        o = pickle.load(proc.stdout)
+    return o
+    
