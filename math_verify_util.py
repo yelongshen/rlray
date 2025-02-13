dc048162b13f0f43c8f2e6c8811c218910089e58
yelongshen <yeshe@microsoft.com>
2025-02-13 00:45:50 -0800

    Update and rename math_verify.py to math_verify_util.py

--- math_verify_util.py
+++ math_verify_util.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import pickle
 
+
 def math_verify(answer, gold):
     command = ['python3.12', '-c', f'from math_verify import parse, verify; import pickle; import sys; pickle.dump(verify(parse("{answer}"), parse("{gold}")), sys.stdout.buffer)']
     o = False
