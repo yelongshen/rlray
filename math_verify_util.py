4139240e7c7616f83537c404707365520a28879c
yelongshen <yeshe@microsoft.com>
2025-02-14 09:48:21 -0800

    Update math_verify_util.py

--- math_verify_util.py
+++ math_verify_util.py
@@ -5,7 +5,7 @@ import subprocess
 import pickle
 
 
-def math_verify(answer, gold):
+def math_verify(gold, answer):
     escaped_answer = answer.replace("\\", "\\\\").replace('"', '\\"')
     escaped_gold = gold.replace("\\", "\\\\").replace('"', '\\"')
     command = ['python3.12', '-c', f'from math_verify import parse, verify; import pickle; import sys; pickle.dump(verify(parse("{escaped_answer}"), parse("{escaped_gold}")), sys.stdout.buffer)']
