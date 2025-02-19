e57e107ba7b674ccc1f4bea65b538439c4ac9948
yelongshen <yeshe@microsoft.com>
2025-02-19 15:03:35 -0800

    Update phi4.py

--- xlmlib/phi4.py
+++ xlmlib/phi4.py
@@ -773,9 +773,9 @@ class _Phi4ForCausalLM(_Phi4PreTrainedModel):
         min_prompt_len = min(len(t) for t in prompt_tokens)
         max_prompt_len = max(len(t) for t in prompt_tokens)
         #assert max_prompt_len <= params.max_seq_len
-        total_len = min(4096, max_gen_len + max_prompt_len)
+        total_len = max_gen_len #+ max_prompt_len)
 
-        pad_id = self.config.pad_token_id
+        pad_id = self.config.eos_token_id # self.config.pad_token_id
         eos_id = self.config.eos_token_id # eos_token_id
         bos_id = self.config.bos_token_id
         
@@ -841,8 +841,7 @@ class _Phi4ForCausalLM(_Phi4PreTrainedModel):
             token_logprobs[:, prev_pos: cur_pos] = -F.cross_entropy(
                 input=logits.reshape(-1, self.vocab_size), #.transpose(1, 2),
                 target=tokens[:, prev_pos + 1 : cur_pos + 1].reshape(-1),
-                reduction="none",
-                ignore_index=pad_id,
+                reduction="none"
             )
             #print('critics.shape', critics.shape)
             #print('token_critics[:, prev_pos: cur_pos].shape', token_critics[:, prev_pos: cur_pos].shape)
