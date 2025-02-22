749c3295f074107ad1280e720763f1e15a931910
yelongshen <yeshe@microsoft.com>
2025-02-21 17:22:32 -0800

    Update replaybuffer.py

--- xlmlib/replaybuffer.py
+++ xlmlib/replaybuffer.py
@@ -71,6 +71,35 @@ class ReplayBuffer:
     def __len__(self):
         return len(self.buffer)
 
+    def calculate_positive_advantage(self, gamma=0.995, group = 8):
+        assert len(self.buffer) % group == 0 
+        rewards = [d.reward for d in self.buffer]
+
+        norm_rewards = []
+        for g in range(0, len(self.buffer) // group):
+            _rewards = rewards[g * group: (g+1) * group]
+            _all_reward = np.sum(_rewards)
+            
+            _norm_rewards = [ (r * 1.0 / _all_reward) for _r in _rewards]  # _norm(_rewards)
+            norm_rewards = norm_rewards + _norm_rewards
+            
+        for idx, d in enumerate(self.buffer):
+            d.norm_reward = norm_rewards[idx]
+
+        for d in self.buffer:
+            #prompt, response, reward, probs, crits, tokens, masks, seq_rewards = d
+            acc_reward = d.reward
+            d.advantages = []
+            d.returns = []
+            for r, c in zip(reversed(d.seq_rewards), reversed(d.crits)):
+                acc_reward = gamma * acc_reward #+ d.norm_reward
+                #advantage = acc_reward # - c
+                d.advantages.insert(0, d.norm_reward)
+                d.returns.insert(0, acc_reward)
+            
+            d.normalized_advantages = d.advantages
+       
+        
     def calculate_group_advantage(self, gamma=0.995, group = 8):
         assert len(self.buffer) % group == 0 
         rewards = [d.reward for d in self.buffer]
