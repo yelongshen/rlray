e0eb8077dc4eccd50de4fbec0a9f9a1cc32c36da
yelongshen <yeshe@microsoft.com>
2025-03-14 09:02:56 -0700

    Update __init__.py

--- xlmlib/__init__.py
+++ xlmlib/__init__.py
@@ -1,4 +1,4 @@
 from .samba import _SambaForCausalLM
 from .replaybuffer import RpcReplayBuffer
-from .math_util import process_math_prompt, process_math_answer
+from .math_util import process_math_prompt, process_math_answer, safe_math_answer_timeout
 from .phi4 import _Phi4ForCausalLM
