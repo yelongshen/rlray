9f2c74808a149207932ffbb34b3d3414fc8cf559
yelongshen <yeshe@microsoft.com>
2025-04-07 14:47:22 -0700

    Update omni_test.py

--- test/omni_test.py
+++ test/omni_test.py
@@ -2,6 +2,36 @@
 import librosa
 from transformers import WhisperFeatureExtractor, WhisperTokenizer
 
+#  "chunk_length": 300,
+#  "dither": 0.0,
+#  "feature_extractor_type": "WhisperFeatureExtractor",
+#  "feature_size": 128,
+#  "hop_length": 160,
+#  "image_mean": [
+#    0.48145466,
+#    0.4578275,
+#    0.40821073
+#  ],
+#  "image_processor_type": "Qwen2VLImageProcessor",
+#  "image_std": [
+#    0.26862954,
+#    0.26130258,
+#    0.27577711
+#  ],
+#  "max_pixels": 12845056,
+#  "merge_size": 2,
+#  "min_pixels": 3136,
+#  "n_fft": 400,
+#  "n_samples": 4800000,
+#  "nb_max_frames": 30000,
+#  "padding_side": "right",
+#  "padding_value": 0.0,
+#  "patch_size": 14,
+#  "processor_class": "Qwen2_5OmniProcessor",
+#  "return_attention_mask": true,
+#  "sampling_rate": 16000,
+#  "temporal_patch_size": 2
+
 if __name__ == "__main__":
     path = './question1.m4a'
     audio_data, _ = librosa.load(path, sr=16000)
@@ -11,14 +41,19 @@ if __name__ == "__main__":
     feature_extractor_3 = WhisperFeatureExtractor.from_pretrained("openai/whisper-medium")
 
     inputs_1 = feature_extractor_1(audio_data, sampling_rate=16000, return_tensors="pt")
-    print(inputs_1['input_features'].shape)
-
-    inputs_2 = feature_extractor_2(audio_data, sampling_rate=16000, return_tensors="pt")
-    print(inputs_2['input_features'].shape)
-
     inputs_3 = feature_extractor_3(audio_data, sampling_rate=16000, return_tensors="pt")
-    print(inputs_3['input_features'].shape)
-    
 
+    d = inputs_1['input_features'] - inputs_3['input_features']
+    print(d)
+    print(sum(d*d)/len(d))
+    
+    #assert torch.allclose(o1, o2, atol=1e-1)
+    #assert 
+    #print(inputs_1['input_features'].shape)
+    #inputs_2 = feature_extractor_2(audio_data, sampling_rate=16000, return_tensors="pt")
+    #print(inputs_2['input_features'].shape)
+    #print(inputs_3['input_features'].shape)
+    
+    
 
     
