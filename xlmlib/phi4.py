03e1c3fce34e42b6482228c785fd91f68cbfadf6
yelong shen <yeshe@microsoft.com>
2025-07-01 18:53:53 -0700

    debug2 bs=2

--- xlmlib/phi4.py
+++ xlmlib/phi4.py
@@ -385,6 +385,7 @@ class _Phi4FlashAttention2(_Phi4Attention):
         #    value_cache = value_states
  
         if page_attention:
+            print('context.is_prefill', context.is_prefill)
             if context.is_prefill:
                 attn_output = flash_attn_varlen_func(query_states, self.k_cache, self.v_cache,
                                        max_seqlen_q=context.max_seqlen_q, cu_seqlens_q=context.cu_seqlens_q,
